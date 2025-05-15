34a4019fc4dc442545a99147316b323ef08a765b
Rahul <rahulmishrapu22@gmail.com>
2025-05-15 12:06:28 +0530

    ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Azeret_Mono as Geist_Mono } from 'next/font/google';
 import { Inter } from 'next/font/google';
 import "./globals.css";
 import AppWalletProvider from "./components/AppWalletProvider";
+import { GoogleAnalytics } from 'nextjs-google-analytics';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,18 +34,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4T69WGZT5R"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4T69WGZT5R');
+            `,
+          }}
+        />
+        {/* Other head elements */}
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Anton+SC&display=swap" rel="stylesheet" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} font-sans antialiased`}
       >
-
-
         <header>
-      
+          {/* Header content */}
         </header>
-       
         <AppWalletProvider>{children}</AppWalletProvider>
       </body>
     </html>
